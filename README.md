84cb00dd1b2d5a1b5b2ece45143cd04ab9350626
ZATTARA <svdubovik1990@mail.ru>
2016-09-08 15:16:33 +0300

    creat csvimpot script

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on September 6, 2016, 2:49 pm.
---
+++ src/CSVParceBundle/Entity/Product.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace CSVParceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="CSVParceBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="product_name", type="string", length=255)
+     */
+    private $productName;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="stock", type="integer")
+     */
+    private $stock;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="discontinued", type="smallint")
+     */
+    private $discontinued;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="discontinued_date", type="datetime")
+     */
+    private $discontinuedDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set productName
+     *
+     * @param string $productName
+     * @return Product
+     */
+    public function setProductName($productName)
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    /**
+     * Get productName
+     *
+     * @return string 
+     */
+    public function getProductName()
+    {
+        return $this->productName;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set stock
+     *
+     * @param integer $stock
+     * @return Product
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * Get stock
+     *
+     * @return integer 
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set discontinued
+     *
+     * @param integer $discontinued
+     * @return Product
+     */
+   public function setDiscontinued($discontinued)
+    {
+        $this->discontinued = $discontinued;
+
+        return $this;
+    }
+
+    /**
+     * Get discontinued
+     *
+     * @return integer 
+     */
+   public function getDiscontinued()
+    {
+        return $this->discontinued;
+    }
+
+    /**
+     * Set discontinuedDate
+     *
+     * @param \DateTime $discontinuedDate
+     * @return Product
+     */
+    public function setDiscontinuedDate($discontinuedDate)
+    {
+        $this->discontinuedDate = $discontinuedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get discontinuedDate
+     *
+     * @return \DateTime 
+     */
+    public function getDiscontinuedDate()
+    {
+        return $this->discontinuedDate;
+    }
+}
